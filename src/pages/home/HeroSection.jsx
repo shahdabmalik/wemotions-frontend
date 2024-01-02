9d5685e450bfdda2e7331f503a83276f114ad865
shahdabmalik <shahdab888@gmail.com>
2024-01-02 21:02:31 +0530

    updated hero section styles

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -11,16 +11,17 @@ const HeroSection = () => {
                 <p className=" font-semibold sm:font-bold text-slate-900 dark:text-slate-100 text-2xl xs:text-3xl md:text-4xl" >Moving from cancel culture to a culture in motion. </p>
                 <p className=" mt-2 font-medium " >Communication is key, so why is there no channel for the voice and emotion of the people to reach  leaders and corporations?</p>
                 <p className=" mt-2 font-medium " >Wemotions is a social movement to solve thought, and bring back the town hall, on the global stage. </p>
-                <Link className="mt-2 text-blue-700 dark:text-blue-600 font-medium uppercase  block" to={"/manifesto"} >Read our blueprint for success</Link>
-                <div className="mt-6 flex gap-5 flex-wrap">
-                    <Link to={"/motions"} type="button" className="w-36 xs:w-40 xs:text-base text-sm h-9 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded flex items-center justify-center" >Explore Motions</Link>
-                    <Link to={"/pages"} type="button" className="w-36 xs:w-40 xs:text-base text-sm h-9 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded flex items-center justify-center" >Motions Pages</Link>
+                <div className="mt-6 flex sm:gap-x-5 gap-y-5 flex-wrap">
+                    <Link className="xs:text-base w-full sm:w-auto h-10 px-4 bg-purple-600 hover:bg-purple-700 text-white font-medium rounded flex items-center justify-center " to={"/manifesto"} >Read our blueprint for success</Link>
+                    <div className="flex gap-x-5 items-center w-full sm:w-auto" >
+                        <Link to={"/motions"} type="button" className=" w-full sm:w-auto  px-4 xs:text-base h-10 bg-purple-600 hover:bg-purple-700 text-white font-medium rounded flex items-center justify-center" >Explore Motions</Link>
+                        <Link to={"/pages"} type="button" className="  w-full sm:w-auto px-4 xs:text-base h-10 bg-purple-600 hover:bg-purple-700 text-white font-medium rounded flex items-center justify-center" >Motions Pages</Link>
+                    </div>
                 </div>
             </div>
             <div className="w-full max-w-[280px] xs:max-w-xs sm:max-w-sm md:max-w-md" >
                 <Lottie animationData={heroAnimation} />
             </div>
-
         </div>
     )
 }
