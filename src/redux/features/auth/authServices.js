83c7fe31e5f06a2e559b12a29f9204a8e25b663b
shahdabmalik <shahdab888@gmail.com>
2024-01-09 23:49:19 +0530

    authservices token bug fix

--- src/redux/features/auth/authServices.js
+++ src/redux/features/auth/authServices.js
@@ -3,15 +3,13 @@ import toast from "react-hot-toast";
 import store from "../../store"
 import { SET_LOGIN } from "./authSlice";
 
-const token = window.localStorage.getItem("token")
-
-axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-
 // login user google
 export const loginUserGoogle = async (data) => {
+    const token = window.localStorage.getItem("token")
     try {
-        const response = await axios.post("/auth/google", data)
+        const response = await axios.post("/auth/google", data, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
         return response.data
     } catch (error) {
         console.log(error);
@@ -22,8 +20,11 @@ export const loginUserGoogle = async (data) => {
 
 // login status
 export const loginStatus = async () => {
+    const token = window.localStorage.getItem("token")
     try {
-        const response = await axios.get("/auth/loggedin")
+        const response = await axios.get("/auth/loggedin", {}, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
         return response.data
     } catch (error) {
         console.log(error);
